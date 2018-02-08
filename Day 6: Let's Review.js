44b77cd305246d59085e21ba16fa2351f377448e
Adrian Szlegel <adrian95999@gmail.com>
2018-02-08 10:52:50 +0100

    Create Day 6: Let's Review.js

---
+++ Day 6: Let's Review.js
@@ -0,0 +1,26 @@
+function processData(input) {
+    var m = input.split("\n")
+    var even = "", odd = "";
+    for (var i = 1; i < m.length; i++) {
+        for (var j = 0; j < m[i].length; j++){
+            if (j%2 == 0){
+                even = even + m[i][j];
+            }else {
+                odd = odd + m[i][j];
+            }
+        }
+        console.log(even + " " + odd);
+        even = "", odd = "";
+    }
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
