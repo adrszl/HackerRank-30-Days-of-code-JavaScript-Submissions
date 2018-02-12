091e53ff8c56540a7a9e3a9fa502711127380d46
Adrian Szlegel <adrian95999@gmail.com>
2018-02-12 12:31:57 +0100

    Done :fire:

---
+++ Day 13: Abstract Classes.js
@@ -0,0 +1,28 @@
+// Declare your class here.
+    /**   
+    *   Class Constructor
+    *   
+    *   @param title The book's title.
+    *   @param author The book's author.
+    *   @param price The book's price.
+    **/
+    // Write your constructor here
+    class MyBook extends Book {
+        constructor(title, author, price) {
+            super(title, author);
+            this.price = price;
+        }
+        display() {
+            console.log("Title: " + this.title);
+            console.log("Author: " + this.author);
+            console.log("Price: " + this.price);
+        }
+    }
+    /**   
+    *   Method Name: display
+    *   
+    *   Print the title, author, and price in the specified format.
+    **/
+    // Write your method here
+    
+// End class
